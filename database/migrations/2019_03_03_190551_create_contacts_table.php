68a1eb31cf1b162c0f2716095f207df08c4ef338
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-03-03 20:17:38 +0100

    00

---
+++ database/migrations/2019_03_03_190551_create_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('email');
+            $table->string('telephone')->nullable();
+            $table->string('pays')->nullable();
+            $table->string('adresse')->nullable();
+            $table->string('ville');
+            $table->string('code_postale')->nullable();
+            $table->text('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
